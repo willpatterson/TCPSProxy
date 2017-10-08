39b1e51ae9b1461ed33e0fc91592f648f2f422cd
Will Patterson <willpatterson_git@icloud.com>
2017-10-08 03:02:13 +0000

    UPDATE tcp_proxy.c's docstring

--- tcp_proxy.c
+++ tcp_proxy.c
@@ -1,5 +1,5 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
+/* The worst TCP proxy ever written */
+
 #include <stdlib.h>
 #include <strings.h>
 #include <stdio.h>
