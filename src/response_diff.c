ac302e48023aa4273ec8c246dab2614fb2e416fb
Will Patterson <willpatterson_git@icloud.com>
2017-10-21 21:46:50 +0000

    CREATE start of response diffing functions

---
+++ src/response_diff.c
@@ -0,0 +1,15 @@
+
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct response_vector {
+    char *** responses;
+    int * response_lengths;
+    int length;
+} RESPONSE_VECTOR;
+
+RESPONSE_VECTOR * vector_diff(RESPONSE_VECTOR * responses) {
+
+}
